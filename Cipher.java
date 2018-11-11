025e5b132f489472046778e68c6bca54f17fe20f
AUSB <38762233+AUSB@users.noreply.github.com>
2018-11-11 11:47:09 -0500

    Add files via upload

---
+++ Cipher.java
@@ -0,0 +1,72 @@
+package com.example.yajunbai.kryptonote;
+
+public class Cipher {
+    private String key;
+    private String note;
+
+    public static final String ALPHABET = " ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public Cipher(String k, String n) {
+        this.key = k;
+        this.note = n;
+    }
+
+    public String makePad(String note) {
+        String pad = this.key;
+        for (; pad.length() < note.length(); ) {
+            pad += this.key;
+        }
+        return pad;
+        /*String pad;
+          for(pad = this.key; pad.length() < note.length(); pad += this.key);
+          return pad;
+        */
+    }
+
+    public String encrypt() {
+        String pad = makePad(note);
+        String result = "";
+        for (int i = 0; i < note.length(); i++) {
+            String c = note.substring(i, i + 1);
+            int position = ALPHABET.indexOf(c);
+            int shift = Integer.parseInt(pad.substring(i, i + 1));
+            int newPosition = (position + shift) % ALPHABET.length();
+            result = result + ALPHABET.substring(newPosition, newPosition + 1);
+            /*
+            int newPosition = position + shift;
+            if(newPosition > ALPHABET.length()){
+            newPosition = newPosition - ALPHABET.length() }
+            */
+        }
+        return result;
+    }
+
+    public String decrypt() {
+        String pad = makePad(note);
+        String result = "";
+        for (int i = 0; i < note.length(); i++) {
+            String c = note.substring(i, i + 1);
+            int position = ALPHABET.indexOf(c);
+            int shift = Integer.parseInt(pad.substring(i, i + 1));
+            int newPosition = (position - shift) % ALPHABET.length();
+
+            // newPosition = position - shift;
+            if (newPosition < 0) {
+                newPosition = newPosition + ALPHABET.length();
+            }
+            /* else if(newPosition > ALPHABET.length()){
+            newPosition = newPosition - ALPHABET.length();
+            }*/
+
+            result = result + ALPHABET.substring(newPosition, newPosition + 1);
+
+        }
+        return result;
+    }
+
+}
+
+
+
+
+
